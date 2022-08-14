e39e4dbef2ac6c0c646711b2fcd87c08d9d3d8f2
Stephan Stephan <stephan_scherders@hotmail.com>
2022-08-14 10:39:17 +0200

    Changes

--- src/main/java/org/stephan/Main.java
+++ src/main/java/org/stephan/Main.java
@@ -1,21 +1,12 @@
 package org.stephan;
-
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
+import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
-
-
-
     @Test
-    void Test1 () {
-ChromeDriver driver = new ChromeDriver();
-driver.get ("https://www.google.nl");
-driver.quit();
+    public void Test() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.google.nl");
+        driver.quit();
     }
-
 }
